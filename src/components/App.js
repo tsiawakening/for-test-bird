72ec55966eed2be2c7226f42639687b6485034ae
birdzzz <aaarea123@163.com>
2021-09-18 00:54:14 +0800

    simple netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong>, you are tired, flying pigs .
+        <strong>{results.length}</strong>,I just want tp try something, please, you are tired, flying pigs .
       </p>
       <hr></hr>
       {results
